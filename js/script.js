9713dc6a6e30077e303542ba27caf3e10d036f94
Balvirportfolio <balvirkumar.3090060.aesl@gmail.com>
2024-09-26 23:07:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
-// Smooth Scroll to Top
-const backToTop = document.getElementById('backToTop');
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-window.onscroll = function() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        backToTop.style.display = "block";
-    } else {
-        backToTop.style.display = "none";
-    }
-};
-
-backToTop.addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-
-// Learn More Button Scroll
-document.getElementById('learnMore').addEventListener('click', function() {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
