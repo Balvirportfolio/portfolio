82c36ac1f3b47fcf6258bf8058253d13784f8d65
Balvirportfolio <balvirkumar.3090060.aesl@gmail.com>
2024-09-27 10:30:02 +0530

    Update particle-config.js

--- js/particle-config.js
+++ js/particle-config.js
@@ -1,3 +1,37 @@
-particlesJS.load('particles-js', 'assets/particles-config.json', function() {
-    console.log('Particles.js loaded');
+// Particle.js configuration
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": { "value": "#f39c12" },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": true
+        },
+        "size": {
+            "value": 3,
+            "random": true
+        },
+        "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false
+        }
+    }
 });
